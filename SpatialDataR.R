3371bb63a6bf0ecd92278e5d088ec21843689b6b
NosyShark <emmiswan@gmail.com>
2024-05-22 10:53:51 +0200

    Fisinhed the mode choices by adding a frequency table and added another method of chosing clusters (worse)

--- SpatialDataR.R
+++ SpatialDataR.R
@@ -31,15 +31,16 @@ Alphalist
 ED$kmEast <- 2947 - ED$kmWest
 
 # Create a column for top 20 alpha diversity and others
-ED$highlight <- ifelse(rank(-ED$alpha) <=20, "top20alpha", "other")
+ED$highlight <- ifelse(rank(-ED$alpha) <=20, "chosen", "other")
 
 # Create a scatter plot showing the top 20 eastuaries in terms of alpha diversity
 G1 <- ggplot(ED, aes(x = kmEast, y = alpha)) + 
   geom_point(aes(color = highlight)) +
-  scale_color_manual(values = c("top20alpha" = "#A36DAD", "other" = "#A0D4A1")) +
+  scale_color_manual(values = c("chosen" = "#A36DAD", "other" = "#A0D4A1")) +
   ggtitle("Scatter Plot with Chosen Estuaries Highlighted") +
   xlab("km East") +
-  ylab ("Alpha Diversity")
+  ylab ("Alpha Diversity")+
+  theme(legend.title = element_blank())
 
 G1
 
@@ -52,9 +53,8 @@ ED$alphalist[1:20] <- 1
 # We can use specnumber in the vegan package for this
 # In essense we're sussing the top 20 estuaries by splitting them into biogeographic zones (BZ) and getthing 6 from the WC and 7 from the SC and EC
 # In other word - we want to richness without the abundance
-alphabz <- specnumber(ED[4:148], groups = ED$alphalist)
-alphabz
-# output: 0 = 121; 1 = 116
+alphaout <- specnumber(ED[4:148], groups = ED$alphalist)
+alphaout # output: 0 = 121; 1 = 116 ~ that's 80%
 
 
 ### Chunk 2
@@ -69,22 +69,17 @@ ED$alphabz[207:212] <- 1 # tagging the first 7 from the West Coast
 ED$alphabz
 
 # Then another column for the highlighted BZ x alpha
-ED$highlightBZ <- ifelse(rank(-ED$alphabz) <=20, "top20alphabz", "other")
+ED$highlightBZ <- ifelse(rank(-ED$alphabz) <=20, "chosen", "other")
 
 # Create a plot for BZ x alpha
 G2 <- ggplot(ED, aes(x = kmEast, y = alpha)) + 
   geom_point(aes(color = highlightBZ)) +
-  scale_color_manual(values = c("top20alphabz" = "#A36DAD", "other" = "#A0D4A1")) +
+  scale_color_manual(values = c("chosen" = "#A36DAD", "other" = "#A0D4A1")) +
   ggtitle("Scatter Plot with Chosen Estuaries Highlighted") +
   xlab("km East") +
-  ylab ("AlphaBZ Diversity")
+  ylab ("AlphaBZ Diversity")+
+  theme(legend.title = element_blank())
 G2
-
-# Aaaaand a list of these estuaries
-ED$alphabzlist <- 0
-ED$alphabzlist[1:7] <- 1
-ED$alphabzlist[91:97] <- 1
-ED$alphabzlist[207:212] <- 1
 
 #Extracting the names of these estuaries
 AlphaBZlistEC <- ED[1:7,1]
@@ -96,18 +91,22 @@ AlphaBZlistSC
 AlphaBZlistWC
 
 # How many species are in the alphabz list
-alphabzspn <- specnumber(ED[4:148], groups = ED$alphabzlist)
-alphabzspn
-# output : 0 = 120; 1 = 122
+alphabzout <- specnumber(ED[4:148], groups = ED$alphabzlist)
+alphabzout # output : 0 = 120; 1 = 122 ~ that's 84%
 
 ### Chunk 3
 # We are going to figure out the mid ranges of each species
 # Problem also because some species have a bi modal dbn
-G3 <- ggplot(ED, aes(x = kmEast, y = Bald.glassy)) + 
-  geom_point(aes(color = highlight)) +
-  scale_color_manual(values = c("top20alpha" = "#A36DAD", "other" = "#A0D4A1"))
+
+G3 <- ggplot(ED, aes(x = kmEast, y = Bluespot.mullet)) + 
+  geom_point(aes(color = highlightBZ)) +
+  scale_color_manual(values = c("top20alphabz" = "#A36DAD", "other" = "#A0D4A1"))+
+  theme(legend.title = element_blank())
 G3
 
+
+
+# Chunk 4
 # Order data by kmWest
 ED <- ED[order(-ED$kmWest),]
 ED
@@ -152,4 +151,97 @@ Range
 Mode
 Centre
 
+hist(Centre)
+hist(Mode)
 
+# Getting the top 20 estuaries by best modes
+# get names of estuaries most frequently in modes
+freq <- table(Mode) # get frequencies of different estuaries 
+sortfreq <- sort(freq, decreasing = TRUE) # sort for highest freqs first 
+sortfreq
+top20range <- c(179, 267, 308, 1448, 1034, 1036, 3, 238, 408, 452, 876, 2947, 568, 685, 830, 842, 984, 318, 564, 638) # take top 20 most frequent modes 
+midranges <- ED$Estuary[match(top20range, ED$kmWest)] # match the km values to the estuary name 
+
+# in retro I have taken the top 25 cross referenced the last 8 with the most biodiverse estuaries and found that three corresponded and included them (the last 3) into the list (I shifted 20 to 21)
+midranges
+
+# Creating a mode list
+ED$modelist <- 0
+# List of locations to set modelist to 1
+locations <- c("St Lucia", "Mlalazi", "Matigulu/Nyoni", "Knysna", "Kariega", "Bushmans", "Kosi", "Mfolozi/Msunduzi", "Manzimtoti", "Mkomazi", "Kwelera", "Orange", "Mtentu", "Mngazana", "Ngqusi/Inxaxo", "Great Kei", "Great Fish", "Zinkwasi", "Mzamba", "Mntafufu")
+# Set modelist to 1
+ED <- ED %>%
+  mutate(modelist = ifelse(ED$Estuary %in% locations, 1, 0))
+
+# Highlight 
+ED$highlightmode <- ifelse(rank(-ED$modelist) <=20, "chosen", "other")
+# Create a graph
+G4 <- ggplot(ED, aes(x = kmEast, y = alpha)) + 
+  geom_point(aes(color = highlightmode)) +
+  scale_color_manual(values = c("chosen" = "#A36DAD", "other" = "#A0D4A1"))+
+  ggtitle("Scatter Plot with Chosen Estuaries Highlighted") +
+  xlab("km East") +
+  ylab ("Alpha") +
+  theme(legend.title = element_blank())
+G4
+
+# Check how many species are saved in this
+modeout <- specnumber(ED[4:148], groups = ED$modelist)
+modeout # output: 0 = 107; 1 = 128 ~ that's 88%
+
+
+# Chunk 5
+# Dendogram, split estuaries in each group, choose ten groups, order data by group then by diversity in group, then choose top 2 entries of each group
+## Sppcomplist
+# There is an estuary without fish - we need to subset the data to exclude that one
+# Cannot run a dendogram with EMPTY ESTUARIES
+
+ED <- subset(ED, alpha >0)
+nrow(ED) # 218 left
+
+ED_deco <- decostand(ED[,c(4:148)], method = "total") # "total tell us more about the proportions of the fish that occur in each estuary - instead of just focusing on raw abundance numbers because we're interested in spp comp not spp numbers
+ED_deco # this has taken the numbers and reduced them to proportions
+
+ED_dist <- vegdist(ED_deco, method = "bray") # using the proportion dataset we're working out the dissimilarity bt ea estuary by using the bray-curtis measure of dissimilarity
+ED_dist # gives you the dissimilary number but now need a graph - ask R to dra a dendrogram to compare these data
+
+ED_clust <- hclust(ED_dist, method = "average")
+ED_clust # this in in preparation for the plot
+
+plot(ED_clust, hang = -1, ylab = "Dissimilarity", xlab = "Estuaries")
+
+# Cut a line in the dendrogram to allow for it to branch only 10 times
+slice <- cutree(ED_clust, h = 0.75)
+slice # estuaries sorted into dendogram slices ~ 12 branches
+
+slice <- cutree(ED_clust, h = 0.8)
+slice # cut it to 80% ~ landed up with 10 groups
+hist(slice)
+# Create a new variable
+ED$cut80 <- slice
+
+
+# Now we need to order it by cut then by alpha
+ED <- ED[order(ED$cut80,-ED$alpha),]
+# Tagging chosen ones
+ED$clust <- 0
+ED$clust[c(1, 2, 35, 36, 44, 45, 46, 47, 48, 49, 193, 194, 197, 198, 208, 209, 210, 213, 214, 218)] <- 1
+
+
+# Highlight top 20 clusters
+ED$highlightclust <- ifelse(rank(-ED$clust) <=20, "chosen", "other")
+# Plot
+G5 <- ggplot(ED, aes(x = kmEast, y = alpha)) + 
+  geom_point(aes(color = highlightclust)) +
+  scale_color_manual(values = c("chosen" = "#A36DAD", "other" = "#A0D4A1"))+
+  ggtitle("Scatter Plot with Chosen Estuaries Highlighted") +
+  xlab("km East") +
+  ylab ("Alpha") +
+  theme(legend.title = element_blank())
+G5
+
+# Check the number of spp saved
+clustout <- specnumber(ED[4:148], groups = ED$clust)
+clustout # output: 0 = 127; 1 = 107 ~ 74%
+
+# note that this is good but excludes all estuaries without fish and so will be biased against plants and invertebrates
